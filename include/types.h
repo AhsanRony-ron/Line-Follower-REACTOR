6833e27bd2ac62d190081856e5f537a024145105
AhsanRony-ron <ahsanronykusuma@gmail.com>
2026-06-11 18:56:55 +0700

    yah

--- include/types.h
+++ include/types.h
@@ -101,15 +101,17 @@ struct __attribute__((packed)) GlobalConfig {
 //  DEFAULT VALUES
 // ─────────────────────────────────────────
 
-#define DEFAULT_SPEED1      200
-#define DEFAULT_SPEED2      200
+#define DEFAULT_SPEED1      80
+#define DEFAULT_SPEED2      80
 #define DEFAULT_MAX_PWM     255
-#define DEFAULT_KP          10
-#define DEFAULT_KD          100
+#define DEFAULT_KP          5
+#define DEFAULT_KD          200
 #define DEFAULT_TIMER       10      
 #define DEFAULT_DELAY_MS    50
-#define DEFAULT_BELOK_L     180
-#define DEFAULT_BELOK_R     180
+#define DEFAULT_BELOK_L     60
+#define DEFAULT_BELOK_R     60
+#define DEFAULT_FREE_L      50
+#define DEFAULT_FREE_R      50
 #define DEFAULT_LINE        LINE_AUTO
 #define DEFAULT_TRIGGER     TRIGGER_TIMER
 #define DEFAULT_DECISION    DEC_STOP
